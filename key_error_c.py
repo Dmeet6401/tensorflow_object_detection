09d0c16db4b610c44c1cfc16d51dd71cd5fc7621
meet <desai.meet06@gmail.com>
2022-11-24 15:52:19 +0530

    first commit

---
+++ key_error_c.py
@@ -0,0 +1,9 @@
+#if you find key error c change this class in record generator
+
+
+# TO-DO replace this with label map
+def class_text_to_int(row_label):
+    if row_label == 'class':
+        return 1
+    else:
+        return 0  #changed this line
